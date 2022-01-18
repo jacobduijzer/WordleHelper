b488bbf7b69a0905f1e4c1c963619291cf184f8d
Jacob Duijzer <github@duijzer.com>
2022-01-18 14:26:02 +0100

    Changed question text

--- WordleHelper.ConsoleApp/Program.fs
+++ WordleHelper.ConsoleApp/Program.fs
@@ -10,7 +10,7 @@ let main =
         Console.WriteLine($"Step {i}")
         Console.WriteLine($"Words available {uniqueCharacterWords.Length} words")
         let startWord = WordleSolver.randomWord uniqueCharacterWords
-        Console.WriteLine $"Please start with word: {startWord}"
+        Console.WriteLine $"Please enter word: {startWord}"
         let firstResult = WordleSolver.testWord startWord
         uniqueCharacterWords <- WordleSolver.filterList uniqueCharacterWords firstResult
         Console.WriteLine()
